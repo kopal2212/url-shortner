7099732bfcfd608c367704aacadc5698209785d3
Kopal Gupta <kopal.gupta@MiQs-MBP.Dlink>
2022-01-20 20:00:19 +0530

    added code for the application

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# url-shortner
-URL shortening service made with Python Flask, MongoDB for storage and Docker for deployment.
+## URL Shortener
+
+
+URL Shortener made with Python Flask, MongoDB for storage and Docker for deployment.
+
+The application has 2 methods:
+    
+    1.POST: Shortening a URL -> returns the shortened URL
+    2.GET: Looking up a URL -> returns the original URL
+
+To run application, clone the repository and execute following commands
+
+1. `docker compose build`
+2. `docker compose up`
+
+Once the application is up and running,to test it execute:
+    
+    1. To Shorten a URL:
+    curl --location --request POST 'http://127.0.0.1:5000/api/v1/shorten/' \
+         --form 'link="https://en.wikipedia.org/wiki/Reality#Perception"'
+
+    It will return a response like:
+        {
+          "url": "https://tinyurl.com/ykkld2zk"
+        }
+    
+    Please note in the shortened URL: "https://tinyurl.com/ykkld2zk" ,this "ykkld2zk" is the url_id(indentifier)
+
+    2. To lookup original URL from shortened URL's identifier:
+    curl --location --request GET 'http://localhost:5000/api/v1/lookup/<url_id>'
+
+    With respect to above eg. the curl request for looking up a shortened URL would look like:
+    curl --location --request GET 'http://localhost:5000/api/v1/lookup/ykkld2zk'
+    It will return a response like:
+        {
+          "lookup": {
+            "added": "20-01-2022- 11:25:10",
+            "id": "61e946969fdb0451be7fe55a",
+            "added": "23-08-2012- 13:44:23",
+            "link": "https://en.wikipedia.org/wiki/Reality#Perception",
+            "url_id": "y3uaxp32"
+          }
+        }
+
---
+++ web/app.py
@@ -0,0 +1,71 @@
+
+from flask import Flask, request, abort, jsonify
+import pymongo,pyshorteners,re
+from datetime import datetime
+
+app = Flask(__name__)
+URL = 'https://tinyurl.com/'
+
+
+myclient = pymongo.MongoClient("mongodb://db:27017/") # making a mongo connection. 
+db = myclient['shortUrls'] # shortUrls is the name of the database we would be using
+objects = db['urls'] #urls is the name of the collection
+
+#introductory route bounded with welcome function
+@app.route('/', methods=['GET'])
+def welcome():
+    return 'Welcome to URL Shortner made with Python,Flask and MongoDb!'
+
+#route for original URL look up with identifier(url_id) bounded with lookup function
+@app.route('/api/v1/lookup/<url_id>', methods=['GET'])
+def lookup(url_id=None):
+    if url_id:
+        url = objects.find_one({'url_id': url_id})
+        if url:
+            url['id'] = str(url['_id'])
+            url['added'] = date_to_str(url['added'])
+            del url['_id']
+            return jsonify({'lookup': url}),200
+        else:
+            return "Provide valid URL ID"
+    abort(404)
+
+#route to fetch the original web link from the user and return the shortened URL
+@app.route('/api/v1/shorten/', methods=['POST'])
+def original_to_shorten_url():
+    link = request.form.get('link')
+    if isValidURL(link) == False:
+        return abort(400)
+    url = objects.find_one({'link': link})
+    if url:
+        return jsonify({'url': URL + url['url_id']}),200
+    else:
+        return jsonify({'url': shorten_url(link)}),200
+
+#function to verify if the entered URL is valid or not
+def isValidURL(string):
+    regex = ("((http|https)://)(www.)?" +"[a-zA-Z0-9@:%._\\+~#?&//=]" +"{2,150}\\.[a-z]" +"{2,6}\\b([-a-zA-Z0-9@:%" +"._\\+~#?//&=]*)")
+    a = re.compile(regex)
+    if (string == None):
+        return False
+    if(re.search(a, string)):
+        return True
+    else:
+        return False
+
+#function to shorten the any given URL
+def shorten_url(link) :
+    shortener = pyshorteners.Shortener()
+    url = shortener.tinyurl.short(link)
+    url_id=str(url[20:])
+    objects.insert_one({'url_id': url_id, 'link': link, 'shortened_url': URL+url_id, 'added': datetime.utcnow()})
+    return url
+
+#function to convert date to string
+def date_to_str(obj):
+    if isinstance(obj, datetime):
+        return obj.strftime('%d-%m-%Y- %H:%M:%S')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
